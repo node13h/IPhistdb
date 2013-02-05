b301310c4973d4bf5bbcb22d524246a42e9d1162
Sergej Alikov <sergej.alikov@init.lt>
2013-02-05 17:35:49 +0200

    fix indexes in table definitions

--- sql/aggdb.sql
+++ sql/aggdb.sql
@@ -8,8 +8,10 @@ CREATE TABLE `aggregated` (
   `remote_id` varchar(255) default NULL,
   `giaddr` varchar(15) default NULL,
   PRIMARY KEY  (`id_aggregated`),
-  UNIQUE KEY `ip_started_stopped_IDX` (`ip`,`started`,`stopped`),
+  UNIQUE KEY `ip_started_UNIQUE` (`ip`,`started`),
   KEY `ip_IDX` (`ip`),
-  KEY `mac_IDX` (`mac`)
+  KEY `mac_IDX` (`mac`),
+  KEY `ip_started_stopped_IDX` (`ip`,`started`,`stopped`),
+  KEY `mac_started_stopped_IDX` (`mac`,`started`,`stopped`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
