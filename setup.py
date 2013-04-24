37dc1b4c7ddbccc90a3a720d1daceff39ea690a1
Sergej Alikov <sergej.alikov@init.lt>
2013-04-24 14:34:07 +0300

    simple install script

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='iphistdb',
+      version='1.0',
+      description='interface modules for IPhistDB databases',
+      author='Sergej Alikov',
+      author_email='sergej.alikov@gmail.com',
+      url='https://github.com/node13h/IPhistdb/',
+      packages=['iphistdb'],
+      )
