855a3127b0fabbca26b2849eac07ab750aa6f093
Sergej Alikov <sergej.alikov@init.lt>
2013-04-25 16:16:30 +0300

    add licence

--- processcnr.py
+++ processcnr.py
@@ -1,3 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""Imports IP history data from Cisco Network Registrar to aggregated database"""
+
+# Copyright 2013 Sergej Alikov
+
+# This file is part of IPhistdb.
+
+# IPhistdb is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# IPhistdb is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with IPhistdb.  If not, see <http://www.gnu.org/licenses/>.
+
+
+# Run this file daily on the CNR server
+
 import os
 import sys
 import csv
